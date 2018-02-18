9ee3aff78bb2baf256c30ca0e9e0ae471ef3aa3b
Josh Perry <joshperry@outlook.com>
2018-02-18 19:51:23 +0000

    Made some kind of start menu

--- Game.lua
+++ Game.lua
@@ -7,11 +7,25 @@ local lg = love.graphics
 local Game = class("Game")
 Game:include(stateful)
 
+function Game:initialize()
+	self.background = lg.newImage("graphics/ui/menu.png")
+	self.pressStart = lg.newImage("graphics/ui/start.png")
+	self.runningTime = 0
+	self.pressStartRotation = 0
+end
+
 function Game:draw()
-	lg.print("uh press space to start!", 10, 10)
+	local width = self.pressStart:getWidth()
+	local height = self.pressStart:getHeight()
+
+	lg.draw(self.background, 0, 0)
+	lg.draw(self.pressStart, 500 + (width/2), 500 + (height/2), self.pressStartRotation, 1, 1, width/2, height/2)
 end
 
 function Game:update(dt)
+	self.runningTime = self.runningTime + dt
+
+	self.pressStartRotation = math.sin(self.runningTime) / 4
 end
 
 function Game:keypressed(key, unicode)
