2c94cffe4653e98690a13193b9bb5f6a57a5a20d
Josh Perry <joshperry@outlook.com>
2018-02-19 17:51:45 +0000

    Level 2 adjustments

--- levels/2.lua
+++ levels/2.lua
@@ -3,6 +3,8 @@ local level = {}
 level.data = "levels/2"
 level.image = "levels/2.png"
 
+level.dosh = 250
+
 level.tip = {
 	text = "Upgrading turrets is important!",
 	x = 50,
@@ -28,19 +30,79 @@ level.enemySpawns["red"] = {
 		"blue_blob",
 		"blue_blob",
 		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
+		"blue_blob",
 		"blue_blob"
 	},
-	spawnDelay = 1
+	spawnDelay = 0.5
 }
 
 level.enemySpawns["green"] = {
-	enemies = {},
+	enemies = {
+		"green_blob",
+		"green_blob",
+		"green_blob",
+		"green_blob",
+		"green_blob",
+		"green_blob",
+		"green_blob",
+		"green_blob",
+		"green_blob",
+		"green_blob"
+	},
 	spawnDelay = 3
 }
 
 level.enemySpawns["blue"] = {
-	enemies = {},
-	spawnDelay = 1
+	enemies = {
+		"red_blob",
+		"red_blob",
+		"red_blob",
+		"red_blob",
+		"red_blob",
+		"red_blob",
+		"red_blob",
+		"red_blob",
+		"red_blob",
+		"red_blob",
+		"red_blob",
+		"red_blob",
+		"red_blob",
+		"red_blob",
+		"red_blob",
+		"red_blob"
+	},
+	spawnDelay = 2
 }
 
 return level
